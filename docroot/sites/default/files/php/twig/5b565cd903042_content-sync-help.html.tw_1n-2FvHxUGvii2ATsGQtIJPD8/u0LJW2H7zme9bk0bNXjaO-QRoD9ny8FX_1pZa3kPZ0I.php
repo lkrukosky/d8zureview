38e86c062897be6332493834ca4b50799551c7f4
Liz Krukosky <liz@coolfiretech.com>
2018-07-23 21:42:08 -0400

    commit

---
+++ docroot/sites/default/files/php/twig/5b565cd903042_content-sync-help.html.tw_1n-2FvHxUGvii2ATsGQtIJPD8/u0LJW2H7zme9bk0bNXjaO-QRoD9ny8FX_1pZa3kPZ0I.php
@@ -0,0 +1,79 @@
+<?php
+
+/* modules/contrib/content_sync/templates/content-sync-help.html.twig */
+class __TwigTemplate_e1b9110d9c7258bc8350a2d5f254e1b890f0144d927dd8310ba5a1c14cd7dfbb extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array("attach_library" => 17);
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array('attach_library')
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 17
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->attachLibrary("content_sync/content_sync.help"), "html", null, true));
+        echo "
+";
+        // line 18
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["help"] ?? null), "html", null, true));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/content_sync/templates/content-sync-help.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  47 => 18,  43 => 17,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/contrib/content_sync/templates/content-sync-help.html.twig", "/var/www/d8zureview/docroot/modules/contrib/content_sync/templates/content-sync-help.html.twig");
+    }
+}
